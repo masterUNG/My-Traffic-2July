4554ce4cc6a039c997a23e9e0f18e715b280cb72
masterUNG <phrombutr@gmail.com>
2016-07-03 13:04:42 +0700

    Design UI Detail finish

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -40,6 +41,56 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }   // onClick
+        });
+
+        //Create ListView
+        //1. Setter Array Simple
+        final int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        //2. Call From xml
+        final String[] nameStrings = getResources().getStringArray(R.array.name);
+
+        //3. Call From Java
+        MyData myData = new MyData();   // Create object or instance
+        String[] detailStrings = myData.getDetailStrings();
+
+        //Inheriate to MyAdapter
+        MyAdapter myAdapter = new MyAdapter(this, iconInts, nameStrings, detailStrings);
+        trafficListView.setAdapter(myAdapter);
+
+        //Intent to Detail When Click
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("Name", nameStrings[i]);
+                intent.putExtra("Icon", iconInts[i]);
+                intent.putExtra("Index", i);
+                startActivity(intent);
+
+
+            }   // onItemClick
         });
 
 
